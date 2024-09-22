f8329a989abab7e71b88b3b056386df1886d5fd5
Adam <thomasadam83@hotmail.com>
2024-09-22 08:53:59 -0400

    Added Google settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -100,6 +100,12 @@ class DatabaseSeeder extends Seeder
                 'name' => 'GOOGLE_ACCESS_TOKEN',
                 'value' => '',
             ],[
+                'name' => 'GOOGLE_DRIVE_VIDEOS',
+                'value' => 'STOCK_VIDEO,1351-DxRjX_siFqLzSiQGwT1SzwuXDwel',
+            ],[
+                'name' => 'GOOGLE_DRIVE_PHOTOS',
+                'value' => 'STOCK_PHOTO,1k7lntYThFwsWoheP9_fYoS84vWPCjwj6',
+            ],[
                 'name' => 'BRICKSUM_WORDLIST',
                 'value' => 'brick, stud, tube, plate, slope, tile, technic, axle, gear, minifigure, connector, baseplate, corner, hinge, wedge, beam, bush, pin, element, knob, plate, cylinder, cone, bar, bracket, jumper, window, door, roof, panel, flag, antenna, wheel, arch, container, holder, clip, arm, seat, vehicle, propeller, horn, dish, radar, whip, hose, harpoon, fork, tail, knife, sword, axe, hammer, tool, wrench, screwdriver, spanner, chainsaw, saw, shovel, pickaxe, binoculars, camera, flashlight, lantern, magnifying, glass, compass, map, key, gem, crystal, jewel, coin, treasure, chest, trophy, cup, medal, shield, helmet, visor, goggles, hat, cap, tiara, crown, helmet, headgear, hairpiece, beard, glasses, mask, visor, oxygen, tank, backpack, pack, sack, suitcase, briefcase, crate, barrel, bucket, shovel, rock, stone, brick, slope, head, hairpiece, helmet, visor, hat, hands, torso',
             ],[
